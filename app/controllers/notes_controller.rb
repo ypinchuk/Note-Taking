7c065adbf3d1b0293c6ab6aaaeab56966c5344ee
Yuliya Pinchuk <ypinchuk@uncc.edu>
2018-02-26 18:44:28 +0000

    Complete Create and Show.

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,8 +1,20 @@
 class NotesController < ApplicationController
+    def show
+        @note = Note.find(params[:id])
+    end
+    
     def new
     end
     
     def create
-        render plain: params[:note].inspect
+        @note = Note.new(note_params)
+        
+        @note.save
+        redirect_to @note
     end
 end
+
+private
+    def note_params
+        params.require(:note).permit(:title, :text)
+    end
