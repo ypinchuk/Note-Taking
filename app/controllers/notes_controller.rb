a7ce320b9aafdc90c0393da70424f3d89724dc3e
Yuliya Pinchuk <ypinchuk@uncc.edu>
2018-03-02 02:34:16 +0000

    Add error message for title and text validation

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -8,13 +8,17 @@ class NotesController < ApplicationController
     end
     
     def new
+        @note = Note.new
     end
     
     def create
         @note = Note.new(note_params)
         
-        @note.save
-        redirect_to @note
+        if @note.save
+            redirect_to @note
+        else
+            render 'new'
+        end
     end
 end
 
